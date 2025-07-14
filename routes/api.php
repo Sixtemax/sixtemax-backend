cffd28514eaa913764ca528274ec4a52a6c043f4
Sixtemax <sixtemaxweb@gmail.com>
2025-07-14 13:48:20 -0500

    test api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\TerceroController;
 
+Route::get('/ping', function () {
+    return response()->json(['status' => 'API funcionando correctamente']);
+});
 
 Route::middleware('api')->prefix('terceros')->group(function () {
     Route::get('/', [TerceroController::class, 'index']);
